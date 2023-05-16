d938ed9a3e8374739c1ee1fc08a1eee6158fa9da
bigperson <daein7076@gmail.com>
2023-05-16 17:11:12 +0900

    solve No.11779

---
+++ algorithm/no11779.py
@@ -0,0 +1,51 @@
+from sys import stdin as s
+import heapq
+s = open('input.txt', 'r')
+
+INF = int(10 ** 8)
+city_num = int(s.readline().strip())
+bus_num = int(s.readline().strip())
+distances = [INF] * (city_num + 1)
+graph = [[] for _ in range(city_num + 1)]
+for _ in range(bus_num):
+    start, end, cost = map(int, s.readline().split())
+    graph[start].append((end, cost))
+
+start_city, destination = map(int, s.readline().split())
+predeccessor = [start_city] * (city_num + 1)
+
+def dijkstra(start):
+    heap = []
+    heapq.heappush(heap, (start, 0))
+    distances[start] = 0
+
+    while heap:
+        current, distance = heapq.heappop(heap)
+
+        if distances[current] < distance:
+            continue
+
+        for i in graph[current]:
+            cost = distance + i[1]
+            if cost < distances[i[0]]:
+                distances[i[0]] = cost
+                predeccessor[i[0]] = current 
+                heapq.heappush(heap, (i[0], cost))
+
+dijkstra(start_city)
+
+print_destination = []
+temp = destination
+while temp != start_city:
+    print_destination.append(temp)
+    temp = predeccessor[temp]
+
+# if print_destination[-1] != start_city:
+print_destination.append(start_city)
+# print(print_destination)
+
+print(distances[destination])
+print(len(print_destination))
+for i in range(len(print_destination) - 1, -1, -1):
+    print(print_destination[i], end=' ')
+print()
